0aa248bc8378968cb14367d0f7f5b20f6026a5ef
Mojo Codess <jcakelley@gmail.com>
2018-01-31 22:19:14 -0600

    Added song list code

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,6 +10,7 @@ class Album extends Component{
     });
 
     this.state = { album };
+    //console.log(this.state);
   };
 
   render(){
@@ -30,6 +31,20 @@ class Album extends Component{
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+             {this.state.albums.songs.map( (song, index) =>
+               <tr>
+                <td>
+                 <button>
+                   <span className="song-number">${index+1}</span>
+                   <span className="ion-play"></span>
+                   <span className="ion-pause"></span>
+                 </button>
+                </td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>
+            )
+          }
            </tbody>
          </table>
       </section>
